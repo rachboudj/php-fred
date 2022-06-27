33262d69b74174554afb94b4f4d6f44fe3060118
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-06-27 11:33:58 +0200

    révisions des opérateurs

--- index.php
+++ index.php
@@ -34,7 +34,54 @@ Quelques règles :
 - toutes les variables commencent par une lettre ou '_'
 - le nom d'une variable contient des lettres, chiffres, des underscore
 - le nom d'une variable ne contient pas d'espace
-
-
-
 */
+echo "<br />";
+
+$jour = "mardi";
+$jourTest = $jour = "mercredi";
+
+// Passage par référence
+$a = 1;
+$b = &$a;
+echo $b;
+
+/* Les différents types de variables 
+- String 
+- Integer
+- Float 
+- Boolean
+- NULL
+- Array
+- Object 
+- Ressource 
+- Enumeration ou Enums
+*/
+
+/* Opérateurs 
+- Addition +
+- Soustraction -
+- Multiplication *
+- Division /
+- Modulo %
+- ** (10 puissance 2 : 10 ** 2)
+*/
+
+/* Opérateurs combinés 
+$i = $i + 1;
+$i++;
+$i += 1;
+*/
+$x = $x / 4;
+$x /= 4;
+
+/* Opérateurs de comparaison 
+- == compare la valeur
+- === compare la valeur et le type (utilisé surtout avec les booléans)
+- != ou <> différent en valeur 
+- ! == différent en valeurs et en types
+*/
+
+// Exemple de vérification d'opérateurs de comparaison avec la fonction var_dump()
+$x = 42;
+var_dump($x == "42");
+
