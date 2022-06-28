1ef70b12387f2b37afc107bc079a224263345ebb
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-06-28 11:39:23 +0200

    test de l'autoLoad

--- index2.php
+++ index2.php
@@ -16,30 +16,34 @@ require_once './functions/factorielle.php';
 </head>
 <body>
     <?php
-        // création d'un tableau
-        $tableau = array("Michel", "Banane", 12, true);
+    //     // création d'un tableau
+    //     $tableau = array("Michel", "Banane", 12, true);
         
-        // Création d'un tableau vide
-        $tableau2 = array();
-        // Ajout d'une valeur à la fin du tableau
-        array_push($tableau2, "Toto", "Titi");
+    //     // Création d'un tableau vide
+    //     $tableau2 = array();
+    //     // Ajout d'une valeur à la fin du tableau
+    //     array_push($tableau2, "Toto", "Titi");
         
-        // Tableau associatif
-        $tableauAssociatif = array("prenom" => "Michel", "nom" => "Dupont", "age" => 42);
+    //     // Tableau associatif
+    //     $tableauAssociatif = array("prenom" => "Michel", "nom" => "Dupont", "age" => 42);
 
-        $resultat = "<ul>";
+    //     $resultat = "<ul>";
 
-    for ($i = 0 ; $i < count($tableau2) ; $i++) {
-        $resultat .= "<li>";
-        if (is_array($tableau2[$i]))
-            $resultat .= "C'est un tableau";
-        else
-            $resultat .= $tableau2[$i];
-        $resultat .= "</li>";
-    }    
-    $resultat .= "</ul>";
+    // for ($i = 0 ; $i < count($tableau2) ; $i++) {
+    //     $resultat .= "<li>";
+    //     if (is_array($tableau2[$i]))
+    //         $resultat .= "C'est un tableau";
+    //     else
+    //         $resultat .= $tableau2[$i];
+    //     $resultat .= "</li>";
+    // }    
+    // $resultat .= "</ul>";
     
-    echo $resultat;
+    // echo $resultat;
+
+    $test = surface(10,20);
+    var_dump($test);
+
 
     ?>
 </body>
