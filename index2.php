70768a308d49a95745534be77cb0486231301201
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-06-28 10:03:52 +0200

    tableau php

---
+++ index2.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once './functions/salut.php';
+require_once './functions/surface.php';
+require_once './functions/volume.php';
+require_once './functions/factorielle.php';
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Fonctions</title>
+</head>
+<body>
+    <?php
+        $tableau = array("Michel", "Banane", 12, true);
+        var_dump($tableau);
+
+    ?>
+</body>
+</html>
